14077128f00a3325605e2fb2b3ea4813cf7e6b63
inessilva-4 <inesfsilva04@ua.pt>
2025-10-12 11:10:33 +0100

    chore: configurar ESLint, Prettier e integração com Contentful

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_PATH = "cms-contentful";
+//prefixo _ faz o ESLint ignorar variáveis não usadas.
 export const CMS_NAME = "Contentful";
 export const CMS_URL = "https://www.contentful.com";
